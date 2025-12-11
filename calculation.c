ecbfe824962662cbf8b75c747b71f6bfbd0f8927
karan7594 <karanprajapat723@gmail.com>
2025-12-11 09:42:02 +0530

    sum two number

---
+++ calculation.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    int a,b;
+    printf("enter a first number : ");
+    scanf("%d",&a);
+    printf("enter a second number : ");
+    scanf("%d",&b);
+    int sum =a+b;
+    printf("sum of %d",sum);
+    return 0;
+}
