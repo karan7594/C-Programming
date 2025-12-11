e48a140ad8b26d56a241dda0d23bbdf9a86dc8db
karan7594 <karanprajapat723@gmail.com>
2025-12-11 11:39:52 +0530

    ternary operator and if else conditions  perform

--- practice questions/vote.c
+++ practice questions/vote.c
@@ -4,15 +4,16 @@ int main(){
     int age; 
     printf("enter ege of person : ");
     scanf("%d",&age);
-    if(age>=18 && age<90){
-        printf("you can vote ! ");
-    }
-    else if(age>=90){
-        printf("you can not vote ! ");
-    }
-    else{
-        printf("you can not eligible ");
-    }
+    // if(age>=18 && age<90){
+    //     printf("you can vote ! ");
+    // }
+    // else if(age>=90){
+    //     printf("you can not vote ! ");
+    // }
+    // else{
+    //     printf("you can not eligible ");
+    // }
+    age>=18? printf("you are eligible for vote ") : printf("you are not eligible for vote ");
     return 0 ;
 
 }
