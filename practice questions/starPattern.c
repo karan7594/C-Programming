b096b13704546a3b1ecb9a47855d255ab439d0c0
karan7594 <karanprajapat723@gmail.com>
2025-12-12 11:49:29 +0530

    star pattern

---
+++ practice questions/starPattern.c
@@ -0,0 +1,75 @@
+// #include<stdio.h>
+
+// int main(){
+
+//     for(int i=1;i<=4;i++){
+//         for(int j=1;j<=4;j++){
+//             printf("*");
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+
+// }
+
+
+
+// #include<stdio.h>
+
+// int main(){
+//     for(int i=1;i<=5;i++){
+//         for(int j=1;j<=i;j++){
+//             printf("*");
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+
+// #include<stdio.h>
+
+// int main(){
+//     int n;
+//     printf("enter the value of n : ");
+//     scanf("%d",&n);
+
+//     for(int i=1;i<=n;i++){
+//         for(int j=n;j>=i;j--){
+//             printf("*");
+            
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+
+// #include<stdio.h>
+
+// int main(){
+    
+//     for(int i=1;i<=4;i++){
+//         for(int j=1;j<=4;j++){
+//             if(i==1 || i==4 || j==1 || j==4){
+//                 printf("*");
+//             }
+//             else{
+//                 printf(" ");
+//             }
+            
+//         }
+//         printf("\n");
+        
+//     }
+//     return 0;
+// }
+
+               
+
+
+
+
+
