edd0b6b010f71ee4a502da316baaf2ee80c01687
karan7594 <karanprajapat723@gmail.com>
2025-12-12 11:51:17 +0530

    calculate the reverse table of given input by user

---
+++ practice questions/reverse.c
@@ -0,0 +1,30 @@
+// #include<stdio.h>
+
+// int main(){
+//     int n;
+//     printf("enter the number :");
+//     scanf("%d",&n);
+
+//     for(int i=10;i>=1;i--){          //calculate the reverse table of given number;
+        
+//         printf("%d\n",n*i);
+//     }
+//     return 0;
+// }
+
+
+
+// #include<stdio.h>
+
+// int main(){
+//     int sum =0;
+//     for(int i=5;i<=50;i++){
+//         sum = sum+i;
+        
+//     }
+//     printf("%d\n",sum);
+//     return 0;
+
+// }
+
+
