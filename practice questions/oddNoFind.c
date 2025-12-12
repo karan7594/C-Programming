401dcc23f699105ff4375a370d03de1c60c4b702
karan7594 <karanprajapat723@gmail.com>
2025-12-12 11:49:49 +0530

    find odd number

---
+++ practice questions/oddNoFind.c
@@ -0,0 +1,41 @@
+// #include<stdio.h>
+
+// int main(){
+
+//     int x;
+//     printf("enter the number : ");
+//     scanf("%d",&x);
+
+//     // for(int i=5;i<=50;i++){
+//     //     if()
+//     // }
+
+//     if(x%2 != 0){
+//         printf("the number is odd");
+//     }
+//     else{
+//         printf("number is even");
+//     }
+
+//     return 0;
+
+// }
+
+
+
+//print the 5 to  50 odd number 
+
+// #include<stdio.h>
+
+// int main(){
+
+//     for(int i=5;i<=50;i++){
+//         if(i%2 != 0){
+//             printf("%d\n",i);
+//         }
+        
+//     }
+//     return 0;
+
+// }
+
